a695f530a07f26563be57ef5bf3201ea4f565502
Davi Morais <davi.a.morais@aluno.senai.br>
2025-08-08 15:33:38 -0300

    mod: arquivo principal

---
+++ lista_afazeres/main.py
@@ -0,0 +1,29 @@
+from funcoes import lista_tarefas, lista_concluidas, salvar_arquivo, add_tarefa, remov_tarefa, exibir_tarefas, concluir_tarefa, salvar_lista, exibir_menu
+import json
+
+while True:
+    opcao = exibir_menu()
+
+    if opcao == '1':
+        add_tarefa()
+
+    elif opcao == '2':
+        if len(lista_tarefas) == 0:
+            print('Sua lista está vazia.')
+        else:
+            remov_tarefa()
+
+    elif opcao == '3':
+        print('Suas tarefas são:')
+        exibir_tarefas()
+
+    elif opcao == '4':
+        if lista_tarefas == lista_concluidas:
+            print("Todas as tarefas estão concluídas.")
+        else:
+            concluir_tarefa()
+    elif opcao == '5':
+        print("Saindo do programa...")
+        break
+    else:
+        print("Opção inválida, digite novamente.")
